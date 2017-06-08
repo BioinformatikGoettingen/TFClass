797f4e613314a7d9bd19bbfcb4b79ede2305302e
Jürgen Dönitz <jdo@dragonfly.local>
2017-06-08 13:46:11 +0200

    Start with a dashboard for the general and species specific reports.

---
+++ src/main/java/de/sybig/tfclass/DetailsDashboard.java
@@ -0,0 +1,65 @@
+package de.sybig.tfclass;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CloseEvent;
+import org.primefaces.event.DashboardReorderEvent;
+import org.primefaces.event.ToggleEvent;
+import org.primefaces.model.DashboardColumn;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.DefaultDashboardColumn;
+import org.primefaces.model.DefaultDashboardModel;
+
+/**
+ *
+ * @author juergen.doenitz@bioinf.med.uni-goettingen.de
+ */
+@ManagedBean
+@ViewScoped
+public class DetailsDashboard  implements Serializable {
+     
+    private DashboardModel model;
+     
+    @PostConstruct
+    public void init() {
+        model = new DefaultDashboardModel();
+        DashboardColumn column1 = new DefaultDashboardColumn();
+    
+        column1.addWidget("generalReport");
+                column1.addWidget("human");
+        
+        model.addColumn(column1);
+    }
+    
+    public void handleReorder(DashboardReorderEvent event) {
+        FacesMessage message = new FacesMessage();
+        message.setSeverity(FacesMessage.SEVERITY_INFO);
+        message.setSummary("Reordered: " + event.getWidgetId());
+        message.setDetail("Item index: " + event.getItemIndex() + ", Column index: " + event.getColumnIndex() + ", Sender index: " + event.getSenderColumnIndex());
+         
+        addMessage(message);
+    }
+     
+    public void handleClose(CloseEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Panel Closed", "Closed panel id:'" + event.getComponent().getId() + "'");
+         
+        addMessage(message);
+    }
+     
+    public void handleToggle(ToggleEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, event.getComponent().getId() + " toggled", "Status:" + event.getVisibility().name());
+         
+        addMessage(message);
+    }
+     
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+     public DashboardModel getModel() {
+        return model;
+    }
+}
